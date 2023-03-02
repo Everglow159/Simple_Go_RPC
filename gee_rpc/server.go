672f7ea02bdd5e9a60b3a9359aa52a35d4529b1d
Everglow159 <85716955+Everglow159@users.noreply.github.com>
2023-03-02 16:36:44 +0800

    2023.3.2.16:36
    server.go update

--- gee_rpc/server.go
+++ gee_rpc/server.go
@@ -14,13 +14,16 @@ import (
 	"reflect"
 	"strings"
 	"sync"
+	"time"
 )
 
 const MagicNumber = 0x3bef5c
 
 type Option struct {
-	MagicNumber int        // MagicNumber marks this's a geerpc request
-	CodecType   codec.Type // client may choose different Codec to encode body
+	MagicNumber    int           // MagicNumber marks this's a geerpc request
+	CodecType      codec.Type    // client may choose different Codec to encode body
+	ConnectTimeout time.Duration // 0 means no limit
+	HandleTimeout  time.Duration
 }
 
 var DefaultOption = &Option{
